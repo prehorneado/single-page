387726631d8dcba691f0f6b53a7e4819131f93be
Roberto Carraro <Roberto Carraro>
2016-03-02 01:27:25 -0300

    Agrego las funciones 'goToSection'

--- app/js/controller/home.js
+++ app/js/controller/home.js
@@ -1,6 +1,26 @@
 'use strict';
 angular.module('myApp')
-	.controller('HomeCtrl', function () {
-		console.log("home")
+	.controller('HomeCtrl', function ($scope) {
+		$scope.goToSection1 = function(){
+			$('html, body').animate({
+		        scrollTop: $("#section-1").offset().top - 60
+		    }, 400);
+		}
+		$scope.goToSection2 = function(){
+			$('html, body').animate({
+		        scrollTop: $("#section-2").offset().top - 60
+		    }, 400);
+		}
+		$scope.goToSection3 = function(){
+			$('html, body').animate({
+		        scrollTop: $("#section-3").offset().top - 60
+		    }, 400);
+		}
+		$scope.goToSection4 = function(){
+			$('html, body').animate({
+		        scrollTop: $("#section-4").offset().top - 60
+		    }, 400);
+		}
+
 	});
 
